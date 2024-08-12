b208f35181d9fd5d149ac503c94e0e624c7e5813
tqbduy2303 <baoduy230303@gmail.com>
2024-08-12 23:38:30 +0700

    finish highline blogs

--- app/blog/ImageSlider.tsx
+++ app/blog/ImageSlider.tsx
@@ -1,23 +1,64 @@
-import { ArrowBigLeft, ArrowBigRight } from "lucide-react"
+'use client'
+import { ArrowBigLeft, ArrowBigRight, Circle, CircleDot } from "lucide-react"
 import { useState } from "react"
+
 
 type ImageSliderProps = {
     imageUrls: string[]
 }
 
+
 export function ImageSlider({imageUrls}: ImageSliderProps){
-    const [imageIndex, setImageIndex] = useState(0)
+    const [imageIndex, setImageIndex] = useState(0);
+    function showNextImage(){
+        setImageIndex(index => {
+            if (index === imageUrls.length -1) return 0
+            return index +1
+        })
+    }
+
+    function showPrevImage() {
+        setImageIndex((index) => {
+            if (index === 0) return imageUrls.length - 1;
+            return index - 1;
+        });
+    }
+        
     return(
         <div style={{width:"100%", height: "100%", position:"relative"}}>
-            <img src={imageUrls[imageIndex]}
-            className="img-slider-img"/>
-            <button className="img-slider-btn">
+            <div style={{width:"100%", height: "100%",display: "flex", overflow: "hidden"}}>
+            {imageUrls.map(url => (
+                <img key={url} src={url}
+                className="img-slider-img"
+                style={{translate: `${-100* imageIndex}%`}}
+                />
+            ))}
+            </div>
+            
+            <button onClick={showPrevImage} className="img-slider-btn" style={{left:0}} aria-label="View Previous Image">
                 <ArrowBigLeft/>
             </button>
-            <button className="img-slider-btn">
+            <button onClick={showNextImage} className="img-slider-btn" style={{right:0}} aria-label="View Next Image">
                 <ArrowBigRight/>
             </button>
+
+            <div style={{
+                position: "absolute",
+                bottom: ".5rem",
+                left: "50%",
+                translate: "-50",
+                display: "flex",
+                gap: ".25rem",
+            }}>
+                {imageUrls.map((_, index) =>(
+                    <button key={index} onClick={()=>setImageIndex(index)} aria-label={`View Image ${index}`} className="img-slider-dot-btn">
+                        {index === imageIndex ? <CircleDot/> : <Circle/>}
+                        </button>
+                ))}
+            </div>
         </div>
+        
+       
     )
 }
 
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,9 @@
 import { ImageSlider } from "./ImageSlider";
+import img1 from "../img/image.png"
+import img2 from "../img/3f524a5f-9daf-4350-886f-da6cd5ae683d.png"
+import img3 from "../img/qslf0x12.png"
 
-
-const IMAGES = ["car1", "car2", "car3", "car4", "car5"];
+const IMAGES = [img1.src, img2.src ,img3.src];
 export default function Blog() {
 
     return(
@@ -9,10 +11,10 @@ export default function Blog() {
             <div className="header">
 
             </div>
-            <div className="main-hightlight" style={{
+            <div className="main-hightlight mt-10" style={{
                 maxWidth: "1200px",
-                width: "100%",
-                aspectRatio: "10/6",
+                width: "90%",
+                aspectRatio: "11/6",
                 margin: "0 auto",
 
             }}>
